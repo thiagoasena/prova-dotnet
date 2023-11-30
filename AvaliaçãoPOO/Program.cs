8779615e5fe8586177f1bb7702f50f5e1a2e7021
thiagoasena <thiagosena.d.j@gmail.com>
2023-11-30 16:19:33 -0300

    Fazendo Classe Pessoa + validando cpf

--- AvaliaçãoPOO/Program.cs
+++ AvaliaçãoPOO/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+class Pessoa
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string CPF { get; set; }
+
+    public Pessoa (string Nome, DateTime DataNascimento, string CPF)
+    {
+        Nome = Nome; 
+        DataNascimento = DataNascimento;
+        CPF = ValidarCPF(cpf) ? CPF : throw new ArgumentException("CPF");
+    }
+
+    private bool ValidarCPF (string cpf)
+    {
+        if (String.IsNullOrWhiteSpace(cpf))
+        return false;   
+
+        cpf = cpf.Replace(".", "").Replace("-", "");
+
+        if (cpf.Length != 11)
+        return false;
+
+        if(!cpf.All(char.IsDigit))
+        return false;
+    }
+}
